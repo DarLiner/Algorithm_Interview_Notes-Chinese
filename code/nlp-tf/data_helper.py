d6cd98c92765050acd5fb3f41a775e9c6c138cfe
HUA Yang <imhuay@163.com>
2018-07-28 12:53:35 +0800

    update

--- code/nlp-tf/data_helper.py
+++ code/nlp-tf/data_helper.py
@@ -1,7 +1,19 @@
 """"""
 import numpy as np
-import gensim
+from gensim.models.word2vec import Word2Vec
 
 
 def gen_batch_sg(data, batch_size=8, skip_window=1):
-    """"""
+    """
+
+    Args:
+        data:
+            [[doc1], [doc2], ...]
+        batch_size:
+        skip_window:
+
+    Returns:
+
+    """
+
+gen_batch_sg()
