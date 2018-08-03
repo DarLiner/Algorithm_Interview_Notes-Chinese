842387c1e4a27b9ed0507068800211a094644c7e
HUA Yang <imhuay@163.com>
2018-08-03 10:10:45 +0800

    [update] 倒排索引

--- code/nlp-base/inverse_index.py
+++ code/nlp-base/inverse_index.py
@@ -6,6 +6,9 @@ import os
 import sys
 import nltk
 import glob
+
+from itertools import combinations
+
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import word_tokenize
 from nltk import PorterStemmer
@@ -17,54 +20,6 @@ tokenizer = RegexpTokenizer(r'\w+')
 stemmer = PorterStemmer()
 """ 词干提取器
 """
-
-
-def combine_indexes(tokens, files):
-    index, freq_word = create_inverse_index(files)
-
-    ret = dict()
-    # sum_freq = 0
-    # index_list = []
-    # print(tokens)
-    ret_set = []
-    for term in tokens:
-        if term in index.keys():
-            ret[term] = index[term]
-            # print("Term is " + str(term))
-            # print("Index term " + str(index[term]))
-            # index_list.append(index[term])
-            # sum_freq = sum_freq + freq_word[term]
-        else:
-            ret[term] = set()
-
-    ret_sets = list(ret.values())
-    ret["__∩__"] = set.intersection(*ret_sets)
-
-    # print("Index list " + str(index_list))
-
-    # if sum_freq:
-    #     index_result = list(set.intersection(*index_list))  # 求交集
-    #     print("Index result is " + str(index_result))
-    #     return index_result, sum_freq
-    # else:
-    #     return ["No results found"], 0
-    return ret
-
-
-def input_parse(word):
-    word = word.strip()
-    if ',' in word:
-        words_list = word.split(',')
-    elif ' ' in word:
-        words_list = word.split(' ')
-    elif ';' in word:
-        words_list = word.split(';')
-    elif ':' in word:
-        words_list = word.split(':')
-    else:
-        words_list = [word]
-
-    return words_list
 
 
 def word_clean(word):
@@ -103,23 +58,72 @@ def create_inverse_index(files_list):
     return index, word_freq
 
 
-def search(txt, files_list):
-    # tokens = input_parse(term)
+# def get_all_subset(tokens: list):
+#     """获取 tokens 的所有非空子集"""
+#     tokens = set(tokens)
+#
+#     ret = []
+#     for i in range(1, len(tokens) + 1):
+#         ret.extend(list(combinations(tokens, i)))
+#
+#     return ret
+
+
+def search_tokens(tokens, inverse_index, word_freq=None):
+    """"""
+    ret = dict()
+    for term in tokens:
+        if term in inverse_index:
+            ret[frozenset([term])] = inverse_index[term]
+        else:
+            ret[frozenset([term])] = set()
+    return ret
+
+
+def search(txt, inverse_index, word_freq=None):
+    """
+
+    Args:
+        txt(str):
+        inverse_index(dict):
+        word_freq(dict):
+
+    Returns:
+        dict
+    """
     tokens = tokenizer.tokenize(txt)
     tokens = [word_clean(token) for token in tokens]
-    index_result, sum_freq = combine_indexes(tokens, files_list)
-    return index_result, sum_freq
+
+    ret = search_tokens(tokens, inverse_index, word_freq)
+
+    for i in range(2, len(tokens) + 1):
+        for ts in combinations(tokens, i):
+            ret[frozenset(ts)] = set.intersection(*[ret[frozenset([t])] for t in ts])
+
+    # tokens_list = get_all_subset(tokens)
+    # for tokens in tokens_list:
+    #     ret[tokens] = search_tokens(tokens, inverse_index, word_freq)
+
+    return ret
 
 
-def get_all_subset(tokens: list):
-    """获取 tokens 的所有非空子集"""
-    tokens = list(set(tokens))
+def printy(ret):
+    """
 
-    ret = []
-    for i in range(len(tokens))
-        for j in range(i, )
+    Args:
+        ret(dict):
 
+    Returns:
 
+    """
+    for k, v in ret.items():
+        tokens = list(k)
+        print(tokens)
+        if v:
+            for t in v:
+                print('\t', t)
+        else:
+            print('\t', '---')
 
 
 if __name__ == '__main__':
@@ -127,16 +131,28 @@ if __name__ == '__main__':
     files = ['a.txt', 'b.txt', 'c.txt']
     files = [os.path.join(dir_path, file) for file in files]
 
-    index, freq_word = create_inverse_index(files)
+    inverse_index, word_freq = create_inverse_index(files)
+    # print(inverse_index)
+
     search_txt = 'html a z'
-    tokens = tokenizer.tokenize(search_txt)
-    tokens = [word_clean(token) for token in tokens]
-
-    # index_result, sum_freq = search(search_txt, files)
-
-    ret = combine_indexes(tokens, files)
-
-    print(ret)
-    """ 
-    
+    ret = search(search_txt, inverse_index, word_freq)
+    # print(ret)
+    printy(ret)
+    r""" 
+    ['html']
+         ./data\b.txt
+         ./data\c.txt
+    ['a']
+         ./data\b.txt
+         ./data\a.txt
+    ['z']
+         -
+    ['a', 'html']
+         ./data\b.txt
+    ['html', 'z']
+         -
+    ['a', 'z']
+         -
+    ['a', 'html', 'z']
+         -
     """
