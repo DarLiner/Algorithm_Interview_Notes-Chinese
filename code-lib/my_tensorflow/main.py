aef4fac733255525e4524e9e1166fbe2d70e95bb
HUA Yang <imhuay@163.com>
2018-08-12 22:36:28 +0800

    【add】my_tensorflow

--- code-lib/my_tensorflow/main.py
+++
@@ -1,9 +0,0 @@
-from src.utils import tf_dtype
-
-print(tf_dtype)
-
-import tensorflow as tf
-
-sess = tf.Session(graph=tf.get_default_graph())
-
-import tensorlayer as tl
--- code-lib/my_tensorflow/src/layers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .dense import *
-from .highway import *
