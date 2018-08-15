a84917e3c2aade126e03c53bdc15066c13cd840e
HUA Yang <imhuay@163.com>
2018-08-15 15:38:24 +0800

    【update】attention_flow匹配层

--- code/my_tensorflow/src/utils/__init__.py
+++ code/my_tensorflow/src/utils/__init__.py
@@ -11,7 +11,6 @@ from .math_op import *
 import tensorflow as tf
 from ..regularizers import l2_regularizer
 
-
 tf_dtype = tf.float32
 zeros = tf.initializers.zeros
 truncated_normal = tf.initializers.truncated_normal
@@ -25,20 +24,24 @@ def get_wb(shape,
            w_initializer=truncated_normal,
            b_initializer=zeros,
            w_regularizer=l2_regularizer,
-           b_regularizer=l2_regularizer):
+           b_regularizer=l2_regularizer,
+           name=None):
     """"""
-    W = tf.get_variable('W', shape=shape,
+    name = "" if name is None else name + '_'
+    W = tf.get_variable(name + 'W', shape=shape,
                         dtype=tf_dtype, initializer=w_initializer, regularizer=w_regularizer)
-    b = tf.get_variable('b', shape=shape[-1:],
+    b = tf.get_variable(name + 'b', shape=shape[-1:],
                         dtype=tf_dtype, initializer=b_initializer, regularizer=b_regularizer)
     return W, b
 
 
 def get_w(shape,
           w_initializer=truncated_normal,
-          w_regularizer=l2_regularizer):
-    n_in, n_unit = shape
-    W = tf.get_variable('W', [n_in, n_unit], dtype=tf_dtype, initializer=w_initializer, regularizer=w_regularizer)
+          w_regularizer=l2_regularizer,
+          name=None):
+    name = "" if name is None else name + '_'
+    W = tf.get_variable(name + 'W', shape, dtype=tf_dtype, initializer=w_initializer,
+                        regularizer=w_regularizer)
     return W
 
 
